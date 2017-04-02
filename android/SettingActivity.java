74e6d043d2599de8168d4c24c147fe50169cae58
jhmoon1211 <jhmoon1211@gmail.com>
2017-04-02 18:36:37 +0900

    Update SettingActivity.java

--- android/SettingActivity.java
+++ android/SettingActivity.java
@@ -1,25 +1,17 @@
-package kr.ac.kpu.catmint_1;
+package com.moon.picam_test_21;
 
 import android.content.Intent;
-import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.SwitchCompat;
 import android.widget.CompoundButton;
 import android.widget.Switch;
 import android.widget.Toast;
 
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.net.Socket;
-import java.net.UnknownHostException;
-
 public class SettingActivity extends AppCompatActivity {
 
     Switch set_mode;
-    String host, port;
+    String host;
+    Integer port;
     Intent intent;
     final String AUTO_MODE = "auto";
     final String PLAY_MODE = "play";
@@ -33,7 +25,8 @@ public class SettingActivity extends AppCompatActivity {
         intent = getIntent();
 
         host = intent.getStringExtra("HOST");
-        port = intent.getStringExtra("PORT");
+        port = intent.getIntExtra("PORT", 0);
+       // port = intent.getStringExtra("PORT");
 
 
         set_mode = (Switch) findViewById(R.id.switch_mode);
@@ -43,11 +36,11 @@ public class SettingActivity extends AppCompatActivity {
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 if(isChecked == true){
                     Toast.makeText(SettingActivity.this, "switch_on", Toast.LENGTH_SHORT).show();
-                    set_modeTask = new ServerTask(host, Integer.parseInt(port), AUTO_MODE);
+                    set_modeTask = new ServerTask(host, port, AUTO_MODE);
                     set_modeTask.execute();
                 }else{
                     Toast.makeText(getApplicationContext(), "switch_off", Toast.LENGTH_SHORT).show();
-                    set_modeTask = new ServerTask(host, Integer.parseInt(port), PLAY_MODE);
+                    set_modeTask = new ServerTask(host, port, PLAY_MODE);
                     set_modeTask.execute();
                 }
             }
